bcd42037de617accafced4d6c5dce2a2f6d40458
luozhihu_Desktop <2310697151@qq.com>
2022-04-28 09:50:02 +0800

    2022/4/28

--- question/src/com/leecode/Test11.java
+++ question/src/com/leecode/Test11.java
@@ -6,21 +6,39 @@ public class Test11 {
     public static void main(String[] args) throws IOException{
 
     }
-    public static int maxArea(int[] height) {
+//    public static int maxArea(int[] height) {
+//        int n = height.length;
+//        int i = 0,j = n-1;
+//        int max = 0;
+//        while (i<j){
+//            max = Math.max((j-i)*Math.max(height[i],height[j]),max);
+//            if (height[i]==height[j]){
+//                j++;
+//                j++;
+//            }else if (height[i]<height[j]){
+//                i++;
+//            }else {
+//                j++;
+//            }
+//        }
+//        return max;
+//    }
+    public int maxArea(int[] height) {
         int n = height.length;
-        int i = 0,j = n-1;
-        int max = 0;
+        //从两端开始遍历
+        int i = 0,j = n - 1;
+        int ans = 0;
+        //两端相遇，退出循环
         while (i<j){
-            max = Math.max((j-i)*Math.max(height[i],height[j]),max);
-            if (height[i]==height[j]){
-                j++;
-                j++;
-            }else if (height[i]<height[j]){
+            //ans取当前最大值
+            ans = Math.max(ans,(j-i)*Math.min(height[i],height[j]));
+            //根据两端的高度来决定下一步操作
+            if (height[i]<height[j]){
                 i++;
             }else {
-                j++;
+                j--;
             }
         }
-        return max;
+        return ans;
     }
 }
