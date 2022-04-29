21a5f11aa793a48b887c7bcffa6bc6756985b04c
luozhihu_Desktop <2310697151@qq.com>
2022-04-29 11:28:10 +0800

    2022/4/29

--- question/src/com/leecode/Test421.java
+++ question/src/com/leecode/Test421.java
@@ -3,43 +3,94 @@ package com.leecode;
 import java.util.Arrays;
 
 public class Test421 {
-     static int[][] tree = new int[100000*31][2];
-     static int idx = 0;
-    public Test421() {
+//     static int[][] tree = new int[100000*31][2];
+//     static int idx = 0;
+//    public Test421() {
+//        for (int i = 0; i <= idx; i++) {
+//            Arrays.fill(tree[i], 0);
+//        }
+//        idx = 0;
+//        System.out.println("清理");
+//    }
+//    public int findMaximumXOR(int[] nums) {
+//        int ans = 0;
+//        for (int num : nums) {
+//            add(num);
+//            ans = Math.max(ans,MaxX(num));
+//        }
+//        return ans;
+//    }
+//    void add(int x){
+//        int p = 0;
+//        for (int i = 31; i >= 0; i--) {
+//            int a = (x >> i) & 1;
+//            if (tree[p][a] == 0) tree[p][a] = ++idx;
+//            p=tree[p][a];
+//        }
+//    }
+//    int MaxX(int x){
+//        int p = 0;
+//        int ans = 0;
+//        for (int i = 31; i >=0 ; i--) {
+//            int a = (x >> i) & 1,b = 1-a;
+//            if (tree[p][b] != 0) {
+//                ans += 1 << i;
+//                p = tree[p][b];
+//            }else {
+//                ans += 0<<i;
+//                p = tree[p][a];
+//            }
+//        }
+//        return ans;
+//    }
+static int N = 20000;
+    static int idx = 0;
+    //使用二维数组tree[i][j]实现字典树，其中tree[i][0]表示下一位的下标。
+    static int[][] tree = new int[N*32][2];
+    //初始化字典树
+    public Test421(){
         for (int i = 0; i <= idx; i++) {
-            Arrays.fill(tree[i], 0);
+            tree[i][0] = 0;
+            tree[i][1] = 0;
         }
         idx = 0;
-        System.out.println("清理");
+    }
+    //把数加入到字典树中
+    public void add(int x){
+        //每个数都从下标0开始添加
+        int p = 0;
+        //从最高位开始
+        for (int i = 31; i >= 0; i--) {
+            int a = (x >> i) & 1;
+            //如果该位不存在，那么给这位赋值，值为下一位的下标。
+            if(tree[p][a] == 0) tree[p][a] = ++idx;
+            p = tree[p][a];
+        }
+    }
+    public int MaxXor(int x){
+        int p = 0;
+        int ans = 0;
+        for (int i = 31; i >= 0; i--) {
+            //a表示该位的值0或1，b是对a取反
+            int a = (x >> i) & 1,b = 1 - a;
+            //如果b存在,那么把b这位加入到答案
+            if (tree[p][b] != 0){
+                ans += b << i;
+                p = tree[p][b];
+                //如果该位b不存在，那么只能把自己a加入到答案中,因为每个数都有32位，所以每一位上面不是0就是1。
+            }else {
+                ans += a << i;
+                p = tree[p][a];
+            }
+        }
+        return ans ^ x;
     }
     public int findMaximumXOR(int[] nums) {
         int ans = 0;
-        for (int num : nums) {
-            add(num);
-            ans = Math.max(ans,MaxX(num));
-        }
-        return ans;
-    }
-    void add(int x){
-        int p = 0;
-        for (int i = 31; i >= 0; i--) {
-            int a = (x >> i) & 1;
-            if (tree[p][a] == 0) tree[p][a] = ++idx;
-            p=tree[p][a];
-        }
-    }
-    int MaxX(int x){
-        int p = 0;
-        int ans = 0;
-        for (int i = 31; i >=0 ; i--) {
-            int a = (x >> i) & 1,b = 1-a;
-            if (tree[p][b] != 0) {
-                ans += 1 << i;
-                p = tree[p][b];
-            }else {
-                ans += 0<<i;
-                p = tree[p][a];
-            }
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            add(nums[i]);
+            ans = Math.max(ans,MaxXor(nums[i]));
         }
         return ans;
     }
--- question/src/com/leecode/Test502.java
+++ question/src/com/leecode/Test502.java
@@ -3,37 +3,70 @@ package com.leecode;
 import java.util.*;
 
 public class Test502 {
-    public static int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        int n = capital.length;
-        List<int[]> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            list.add(new int[] {capital[i],profits[i]});
-        }
-        list.sort(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] > o2[0]){
-                    return 1;
-                }else if(o1[0] < o2[0]){
-                    return -1;
-                }else {
-                    return o1[1]-o2[1];
-                }
-            }
-        });
-        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2-o1;
-            }
-        });
-        int count = 0;
-        int i = 0;
-        while(count<k){
-            while (i<n&&list.get(i)[0]<=w) queue.offer(list.get(i++)[1]);
-            w = w + queue.poll();
-            count ++;
-        }
-        return w;
+//    public static int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+//        int n = capital.length;
+//        List<int[]> list = new ArrayList<>();
+//        for (int i = 0; i < n; i++) {
+//            list.add(new int[] {capital[i],profits[i]});
+//        }
+//        list.sort(new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                if (o1[0] > o2[0]){
+//                    return 1;
+//                }else if(o1[0] < o2[0]){
+//                    return -1;
+//                }else {
+//                    return o1[1]-o2[1];
+//                }
+//            }
+//        });
+//        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return o2-o1;
+//            }
+//        });
+//        int count = 0;
+//        int i = 0;
+//        while(count<k){
+//            while (i<n&&list.get(i)[0]<=w) queue.offer(list.get(i++)[1]);
+//            w = w + queue.poll();
+//            count ++;
+//        }
+//        return w;
+//    }
+public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+    int n = profits.length;
+    int f[][] = new int[n][2];
+    //把两个数组一一对应，存入二维数组
+    for (int i = 0; i < n; i++) {
+        f[i][0] = capital[i];
+        f[i][1] = profits[i];
     }
+    //安装投资升序排序
+    Arrays.sort(f,(a,b)->{
+        return a[0] - b[0];
+    });
+    int pre = 0;
+    //默认为小根堆，我们需要大根堆，重写compare方法
+    PriorityQueue<Integer> queue = new PriorityQueue<>((a,b)->{
+        return b-a;
+    });
+    //pre记录当前可以做的最大投资的项目的下标
+    for (; pre < n && f[pre][0] <= w; pre++) {
+        queue.add(f[pre][1]);
+    }
+    int i = 0;
+    //如果堆为空表示无项目可做，退出循环
+    while (i<k&&!queue.isEmpty()){
+        i++;
+        //做完的项目需要从堆中移除
+        w = w + queue.poll();
+        for (; pre < n && f[pre][0] <= w; pre++) {
+            queue.add(f[pre][1]);
+        }
+    }
+    return w;
+}
 }
